f4e5356e9cb6b1f6488f0dcb66303b5d9e5368ad
atharv-nagare45 <atharvnagare79@gmail.com>
2026-01-14 20:32:45 -0800

    Rules

--- rules.c.c
+++ rules.c.c
@@ -1,3 +1,5 @@
 //git clone <link>  to open the file in local machine
 //git status is use to see current condition of file
 //git add <file.name>
+//git commit -m "some message here"
+//get push origin main to save it into git
